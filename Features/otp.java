66fdf59d19eeaab3cc7d1896ae284dc9918c9134
Arnesh Rajendran <72863152+Arnesh-02@users.noreply.github.com>
2024-06-05 22:04:39 +0530

    Forgot password feature-restaurent and customer

---
+++ Features/otp.java
@@ -0,0 +1,18 @@
+
+public class  otp(String phone_no){
+    //Code to implemnt otp sending feature
+    private boolean send(){
+        if(//otp send correctly)
+        {
+            try{
+                //sending otp and checking if its true
+                 return true;
+            }
+           
+        }
+        else{
+            return false;
+        }
+    }
+}
+
